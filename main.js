19fbe716a8753126a35e1d712dd0c8fe05dc8360
harbett <haroldbennett16@gmail.com>
2020-11-12 17:15:54 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+const btn = document.getElementsByClassName('btn')[0]
+const menu = document.getElementsByClassName('menu')[0]
+
+btn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('active')
+})
